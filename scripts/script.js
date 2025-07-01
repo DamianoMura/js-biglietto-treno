1dfcb1209ed0bf177ce1fed34abf56ac9ce0fa83
DamianoMura <mura.damiano14@gmail.com>
2025-07-01 13:54:44 +0200

    [add] prompts for age and distance to travel

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,9 @@ let age;
 let distance;
 let price;
 const pricePerKm= 0.21;
+
+
+alert("Benvenuto nella biglietteria di CrazyTrain \n premi ok per selezionare la tratta e la tua età");
+//inseriamo i dati
+distance=parseInt(prompt("inserisci la distanza che vuoi percorrere in km"));
+age=parseInt(prompt("inserisci la tua età"));
